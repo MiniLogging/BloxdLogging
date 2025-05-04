b8af9852246f4669c4a9a85446bd2f1c45abf656
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2025-05-04 08:09:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ def use_regex(input_text):
     pattern = re.compile(r'/[^"]*\.js', re.IGNORECASE)
     return pattern.search(input_text).group()
 
-mainsite = mainsite.replace('/manifest.js','') # Hacky prevent logging (would be better if i cleaned up html)
-mainsite = mainsite.replace('recaptcha.net/recaptcha/api.js','') # Hacky prevent logging (would be better if i cleaned up html)
+# Hacky prevent logging the wrong files
+mainsite = mainsite.replace('/manifest.js','')
+mainsite = mainsite.replace('recaptcha.net/recaptcha/api.js','')
 for line in mainsite.split('\n'):
     if '"/static/js' in line and '.js' in line:
         js_name = use_regex(line.strip())
@@ -18,7 +19,7 @@ for line in mainsite.split('\n'):
         link = f'https://bloxd.io{js_name}'
 
 js_code_raw = requests.get(link)
-changed_day = js_code_raw.headers['date']
+changed_day = js_code_raw.headers['date'] # Sadly no last changed headers
 js_code = js_code_raw.text
 
 # imma save github some work and not deal with sending 50+ requests every hour lmao
@@ -37,8 +38,12 @@ with open('gamecode/main formatted.js','w', encoding="utf-8") as f:
     formatted_code = jsbeautifier.beautify(js_code)
     f.write(formatted_code)
 
-with open('temp_commit_message.txt','w') as f:
-    data = f'{changed_day} {js_name}'
+with open('temp_commit.txt','w') as f:
+    import email.utils as utils
+    parsed_time = utils.parsedate_to_datetime(changed_day)
+    discord_timestamp = int(parsed_time.timestamp())
+    formatted = f"<t:{discord_timestamp}>"
+    data = f'{formatted} {js_name}\n{changed_day}'
     print(data)
     f.write(data)
 #endregion
@@ -46,4 +51,3 @@ with open('temp_commit_message.txt','w') as f:
 prefix = js_name.split('/')[-1].split('.')[0]
 import chunkSaver
 chunkSaver.get_the_chunks(formatted_code,prefix)
-
